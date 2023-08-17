2dab2ddb8b67f4fd49e2b02b5b045df33ff970d3
Neha Thorat <nehathorat2016@gmail.com>
2023-08-17 15:32:57 +0530

    Task Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,33 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args)
+    {
+        Product p = new Product();
+        int ans = p.product(9,9);
+
+        int ans1 =  p.product(9 , 21,21);
+        double ans2 = p.product(1.5,9.5);
+        System.out.println(ans+" "+ans1+" "+ans2);
+
+    }
+    public static class Product
+    {
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z)
+        {
+            return  x*y*z;
+        }
+
+        public double product(double x, double y)
+        {
+            return  x*y;
+        }
+
+    }
+
 }
